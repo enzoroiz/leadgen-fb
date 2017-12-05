36fb0cd306f4c943498124158d8daa556c42ea02
Enzo Roiz <enzoroiz@gmail.com>
2017-12-05 10:51:33 -0200

    Retornando dados enviados para log de erro do Heroku

--- leads.php
+++ leads.php
@@ -6,3 +6,6 @@ $verify_token = $_REQUEST['hub_verify_token'];
 if ($verify_token === 'jcleadgen#2017') {
 	echo $challenge;
 }
+
+$input = json_decode(file_get_contents('php://input'), true);
+error_log(print_r($input, true));
