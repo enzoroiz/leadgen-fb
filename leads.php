43303dd21ea40535935e0f36e533a2654906749e
Enzo Roiz <enzoroiz@gmail.com>
2017-12-05 17:41:42 -0200

    Mensagens de debug

--- leads.php
+++ leads.php
@@ -1,3 +1,12 @@
 <?php
 
-error_log(print_r($_REQUEST), true);
+$challenge = $_REQUEST['hub_challenge'];
+$verify_token = $_REQUEST['hub_verify_token'];
+
+if ($verify_token === 'jcleadgen#2017') {
+	echo $challenge;
+}
+
+$input = json_decode(file_get_contents('php://input'), true);
+error_log(print_r($input, true));
+error_log("testing");
